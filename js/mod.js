7c657e456a3cea07b856c73074026f39d0ce3b02
8bitCreator2 <owencyborg201@gmail.com>
2025-02-27 17:44:09 +0100

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -45,7 +45,7 @@ function getPointGen() {
     let gain = new Decimal(1); // Base gain for points/sec
 
     // Apply the effect of Buyable 14 from the Energy layer
-    if (hasUpgrade("e", 14)) {
+    if (hasBuyable("e", 14)) {
         gain = gain.times(tmp.e.buyables[14].effect);
     }
 
