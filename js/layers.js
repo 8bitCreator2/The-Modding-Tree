cb7bcdd3b35086647e049a90aed537cbe865bf15
8bitCreator2 <owencyborg201@gmail.com>
2025-06-11 13:04:00 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -14,13 +14,46 @@ addLayer("b", {
     type: "none",
 
     update(diff) {
-        let gain = player.points.times(Decimal.add(1, player.b.interest)).div(100);
+        let gain;
+        if (hasUpgrade("b", 11)) {
+            // C = (A / i) * (1 + i) / 100
+            gain = player.points.div(player.b.interest).times(Decimal.add(1, player.b.interest)).div(100);
+        } else {
+            // C = A * (1 + i) / 100
+            gain = player.points.times(Decimal.add(1, player.b.interest)).div(100);
+        }
         player.b.bankingPoints = player.b.bankingPoints.add(gain.times(diff));
+    },
+
+    upgrades: {
+        11: {
+            title: "Improved Formula",
+            description: "Change formula to C = (A / i) × (1 + i) / 100",
+            cost: new Decimal(200),
+            currencyDisplayName: "Banking Points",
+            currencyInternalName: "bankingPoints",
+            unlocked() {
+                return player.b.bankingPoints.gte(200);
+            },
+            effect() {
+                return true;
+            },
+            style() {
+                return {"background-color": "#f5d442", color: "black"};
+            },
+        },
     },
 
     tabFormat: [
         ["display-text", () => `<h2 style='color: white;'>Welcome to the Bank layer.</h2>`],
         ["display-text", () => `You have <b style='color: #f5d442;'>${format(player.b.bankingPoints)}</b> Banking Points.`],
-        ["display-text", () => `Formula: <b>C = A × (1 + i) / 100</b><br>Where A = Points = <b style='color: #f5d442;'>${format(player.points)}</b>, i = <b style='color: #f5d442;'>${format(player.b.interest.mul(100))}%</b>, C = Banking Points gained/sec`],
+        ["display-text", () => {
+            if (hasUpgrade("b", 11)) {
+                return `Formula: <b>C = (A / i) × (1 + i) / 100</b><br>Where A = Points = <b style='color: #f5d442;'>${format(player.points)}</b>, i = <b style='color: #f5d442;'>${format(player.b.interest.mul(100))}%</b>, C = Banking Points gained/sec`;
+            } else {
+                return `Formula: <b>C = A × (1 + i) / 100</b><br>Where A = Points = <b style='color: #f5d442;'>${format(player.points)}</b>, i = <b style='color: #f5d442;'>${format(player.b.interest.mul(100))}%</b>, C = Banking Points gained/sec`;
+            }
+        }],
+        "upgrades",
     ],
 });
