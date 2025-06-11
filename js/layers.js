d9d280c54ec56b666b542add2b2a63d0dcc0d67f
8bitCreator2 <owencyborg201@gmail.com>
2025-06-11 17:38:17 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -16,19 +16,23 @@ addLayer("b", {
     type: "none",
 
     update(diff) {
-        let gain;
-        if (hasUpgrade("b", 11)) {
-            gain = player.points.div(player.b.interest).times(Decimal.add(1, player.b.interest)).div(100);
-        } else {
-            gain = player.points.times(Decimal.add(1, player.b.interest)).div(100);
-        }
+        const A = player.points;
+        let i = player.b.interest;
+
+        if (hasUpgrade("b", 12)) i = i.add(0.02);
+        if (hasUpgrade("b", 13)) i = i.times(Decimal.pow(A.add(1), 0.1));
+
+        const Ai = A.times(i);
+        const bonus = Decimal.add(1, Ai.div(A.div(10)));
+        const gain = A.times(i.add(1)).times(bonus).div(100);
+
         player.b.bankingPoints = player.b.bankingPoints.add(gain.times(diff));
     },
 
     upgrades: {
         11: {
             title: "Improved Formula",
-            description: "Change formula to C = (A / i) × (1 + i) / 100",
+            description: "Improves banking point formula.",
             cost: new Decimal(200),
             canAfford() {
                 return player.b.bankingPoints.gte(this.cost);
@@ -36,12 +40,26 @@ addLayer("b", {
             pay() {
                 player.b.bankingPoints = player.b.bankingPoints.sub(this.cost);
             },
-            unlocked() {
-                return true;
-            },
-            style() {
-                return {"background-color": "#f5d442", color: "black"};
-            },
+            unlocked: () => true,
+            style: () => ({ "background-color": "#f5d442", color: "black" }),
+        },
+        12: {
+            title: "+2% Interest",
+            description: "Interest is increased by 2%.",
+            cost: new Decimal(500),
+            currencyDisplayName: "Banking Points",
+            currencyInternalName: "bankingPoints",
+            unlocked: () => true,
+            style: () => ({ "background-color": "#f5d442", color: "black" }),
+        },
+        13: {
+            title: "Point-Boosted Interest",
+            description: "Your points boost interest (interest × A^0.1).",
+            cost: new Decimal(1000),
+            currencyDisplayName: "Banking Points",
+            currencyInternalName: "bankingPoints",
+            unlocked: () => true,
+            style: () => ({ "background-color": "#f5d442", color: "black" }),
         },
     },
 
@@ -49,11 +67,13 @@ addLayer("b", {
         ["display-text", () => `<h2 style='color: white;'>Welcome to the Bank layer.</h2>`],
         ["display-text", () => `You have <b style='color: #f5d442;'>${format(player.b.bankingPoints)}</b> Banking Points.`],
         ["display-text", () => {
-            if (hasUpgrade("b", 11)) {
-                return `Formula: <b style='color: white;'>C = (A / i) × (1 + i) / 100</b><br>Where A = Points = <b style='color: #f5d442;'>${format(player.points)}</b>, i = <b style='color: #f5d442;'>${format(player.b.interest.mul(100))}%</b>, C = Banking Points gained/sec`;
-            } else {
-                return `Formula: <b style='color: white;'>C = A × (1 + i) / 100</b><br>Where A = Points = <b style='color: #f5d442;'>${format(player.points)}</b>, i = <b style='color: #f5d442;'>${format(player.b.interest.mul(100))}%</b>, C = Banking Points gained/sec`;
-            }
+            const A = player.points;
+            let i = player.b.interest;
+            if (hasUpgrade("b", 12)) i = i.add(0.02);
+            if (hasUpgrade("b", 13)) i = i.times(Decimal.pow(A.add(1), 0.1));
+            const Ai = A.times(i);
+            const bonus = Ai.div(A.div(10));
+            return `Formula: <b style='color: white;'>C = A × (1 + i) × (1 + Ai / (A / 10)) / 100</b><br>Where A = <b style='color: #f5d442;'>${format(A)}</b>, i = <b style='color: #f5d442;'>${format(i.mul(100))}%</b>, Bonus = <b style='color: #f5d442;'>${format(bonus)}</b>`;
         }],
         "upgrades",
     ],
