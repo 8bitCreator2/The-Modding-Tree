e9f31e38d94949e3888610b907eade9425a8a789
8bitCreator2 <owencyborg201@gmail.com>
2025-06-11 12:50:16 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,57 +1,26 @@
-
+// layers/b.js
 addLayer("b", {
     name: "Bank",
     symbol: "B",
     position: 0,
     startData() { return {
         unlocked: true,
-        points: new Decimal(0), // Not used for prestige in this layer
-        deposit: new Decimal(0),
-        i: 0.03,
-        D: 100,
+        points: new Decimal(0),
+        bankingPoints: new Decimal(0),
+        interest: new Decimal(0.05),
     }},
     color: "#f5d442",
     row: 0,
-    type: "none", // Passive layer
+    type: "none",
+
+    update(diff) {
+        let gain = player.points.times(Decimal.add(1, player.b.interest)).div(100);
+        player.b.bankingPoints = player.b.bankingPoints.add(gain.times(diff));
+    },
 
     tabFormat: [
-        ["display-text", function() { return `You have <b>${format(player.b.deposit)}</b> Points in the Bank.` }],
-        ["display-text", function() {
-            let gain = player.b.deposit.times(1 + player.b.i).div(player.b.D).floor();
-            return `Withdrawing now would give <b>${format(gain)}</b> Points.`;
-        }],
-        "blank",
-        ["clickable", 0],
-        ["clickable", 1],
+        ["display-text", () => `<h2 style='color: #f5d442;'>Welcome to the Bank layer.</h2>`],
+        ["display-text", () => `<span style='color: #f5d442;'>You have <b>${format(player.b.bankingPoints)}</b> Banking Points.</span>`],
+        ["display-text", () => `<span style='color: #f5d442;'>Formula: <b>C = A × (1 + i) / 100</b><br>Where A = Points = ${format(player.points)}, i = ${format(player.b.interest.mul(100))}%, C = Banking Points gained/sec</span>`],
     ],
-
-    clickables: {
-        0: {
-            title: "Deposit",
-            display() {
-                return `Deposit 10% of your current Points\nCost: ${format(player.points.div(10))}`;
-            },
-            canClick() { return player.points.gte(10) },
-            onClick() {
-                let amount = player.points.div(10);
-                player.points = player.points.sub(amount);
-                player.b.deposit = player.b.deposit.add(amount);
-            },
-            style: { width: "200px", height: "100px" },
-        },
-        1: {
-            title: "Withdraw",
-            display() {
-                let gain = player.b.deposit.times(1 + player.b.i).div(player.b.D).floor();
-                return `Withdraw your deposit\nGain: ${format(gain)} Points`;
-            },
-            canClick() { return player.b.deposit.gt(0) },
-            onClick() {
-                let gain = player.b.deposit.times(1 + player.b.i).div(player.b.D).floor();
-                player.points = player.points.add(gain);
-                player.b.deposit = new Decimal(0);
-            },
-            style: { width: "200px", height: "100px" },
-        },
-    },
 });
