3786d90cc88b8713a8e642ccbd68e00ce2d84bae
8bitCreator2 <owencyborg201@gmail.com>
2025-06-14 22:40:59 +0200

    Update layers2.js

--- js/layers2.js
+++ js/layers2.js
@@ -8,10 +8,10 @@ addLayer("w", {
     }},
     color: "#A52A2A",
     requires: new Decimal(100000), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    resource: "Wood", // Name of prestige currency
+    baseResource: "Knowledge", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.1, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
@@ -22,7 +22,7 @@ addLayer("w", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "w", description: "W: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "w", description: "W: Reset for wood", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return hasMilestone("p", 1);}
 })
