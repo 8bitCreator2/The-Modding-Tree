c3985e9ed2f0993380218fa7e70c1aa755e562f1
8bitCreator2 <owencyborg201@gmail.com>
2025-06-20 13:28:40 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,15 +1,15 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("n", {
+    name: "Normal Energy", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "N", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#4BDC13",
+    color: "#f5d769",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    resource: "Energetic Points", // Name of prestige currency
+    baseResource: "Energy", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
@@ -24,5 +24,28 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
+    layerShown(){return true},
+	upgrades: { 
+		1: {
+		title: "Energetic Power",
+		description:"3x Energy",
+		cost: new Decimal(1),
+		},
+		2: { 
+		title: "Energetic Power II",
+		description: "2x Normal Energy",
+		cost: new Decimal(5),
+			},
+		3: { 
+		title: "Energetic Power II",
+		description: "Normal energy boosts Energy",
+		cost: new Decimal(5),
+		effect() { 
+			let eff = player.n.points.add(1).pow(0.35)
+			 return eff
+			},
+			effectDisplay() { return format(upgradeEffect("n", 3))
+		},
+			},
+		},
 })
