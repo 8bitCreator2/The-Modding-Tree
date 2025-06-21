040d7f747b3e92770808b082433c2aa3e2c7f40b
8bitCreator2 <owencyborg201@gmail.com>
2025-06-21 09:47:44 +0200

    Create layers2.js

---
+++ js/layers2.js
@@ -0,0 +1,29 @@
+addLayer("t", {
+    name: "Thermal Energy", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "T", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#FA661C",
+    requires: new Decimal(2e6), // Can be a function that takes requirement increases into account
+    resource: "Thermal Energetic Points", // Name of prestige currency
+    baseResource: "Energy", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.1, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "t", description: "T: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){let shown = false
+                if (hasUpgrade("n", 33) || player.t.unlocked)} shown = true
+})
